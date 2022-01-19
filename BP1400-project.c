5ecd98c265c24c81f03a397e28ecb6f95c30b706
Atid khodaie <amirmahdijafarian@gmail.com>
2022-01-19 09:18:59 +0330

    add some chert o perts

--- BP1400-project.c
+++ BP1400-project.c
@@ -4,18 +4,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include <unistd.h>
+#include <stdbool.h>
 
 // defines
 #define normal_limit 50
 
 
 // function declaration
-void  mainPageLanding();
+void clearScreen();
 void forgotPassword();
+void login();
 void adminPageLanding();
 void normalUserLanding();
-
+void addTeam();
+void addPlayer();
+void showTeams();
+void showPlayers();
+void itemFive();
+void  mainPageLanding();
 
 // variables
 char email[normal_limit];
@@ -28,8 +35,32 @@ int main() {
 }
 
 // functions
+void clearScreen() {
+    const char *CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
+    write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+}
 void mainPageLanding() {
-    printf("Welcome sir, choose options below\n1- Login(L)\n2- Forgot password(f)\n");
+    clearScreen();
+    printf("Welcome sir, choose options below\n1- Login\n2- Forgot password\n3- Exit\n");
+    while (1) {
+        int userInput = 0;
+        scanf("%d",&userInput);
+        if (userInput == 1) {
+            login();
+            break;
+        }
+        else if (userInput == 2) {
+            forgotPassword();
+            break;
+        }
+        else if (userInput == 3) {
+            break;
+        }
+        else {
+            printf("Not valid input, try again...\n");
+        }
+    }
+    printf("See you soon!");
 }
 void forgotPassword() {
     printf("For recover your account you have to enter your E-mail\n");
@@ -46,8 +77,50 @@ void login() {
     }
 }
 void adminPageLanding() {
-    printf("Choose an option:\n1- Add team(add)\n2- add player(addp)\n3- show teams(showt)\n4- show players(showp)\n5- item 5(it5)");
+    clearScreen();
+    int userInput = 0;
+    printf("Choose an option by typing its number:\n1- Add team\n2- add player\n3- show teams\n4- show players\n5- item 5\n6- Log out\n");
+    while (1) {
+        scanf("%d",&userInput);
+        if (userInput == 1) {
+            addTeam();
+        }
+        else if (userInput == 2) {
+            addPlayer();
+        }
+        else if (userInput == 3) {
+            showTeams();
+        }
+        else if (userInput == 4) {
+            showPlayers();
+        }
+        else if (userInput == 5) {
+            itemFive();
+        }
+        else if (userInput == 6) {
+            break;
+        }
+        else {
+            printf("Not valid input, try again...\n");
+        }
+    }
+    mainPageLanding();
 }
 void normalUserLanding() {
     printf("Choose an option:\n1- Buy player(buy)\n2- Sell player(sell)\n3- Select team/submit team(steam)\n4- Show scoreboard(scoreboard)\n5- Fixtures(fixt)\n6- Upcoming opponent(upop)\n7- Change password(changepsw)\n8- Exit(wq)");
+}
+void addTeam() {
+    printf("Successfully worked! clicked Item: 1\n");
+}
+void addPlayer() {
+    printf("Successfully worked! clicked Item: 2\n");
+}
+void showTeams() {
+    printf("Successfully worked! clicked Item: 3\n");
+}
+void showPlayers() {
+    printf("Successfully worked! clicked Item: 4\n");
+}
+void itemFive() {
+    printf("Successfully worked! clicked Item: 5\n");
 }
